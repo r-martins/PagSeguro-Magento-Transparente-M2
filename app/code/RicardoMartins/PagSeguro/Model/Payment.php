5016a389aff205fbc0609343ff705268142cfa7f
Ricardo Martins <ricardo@ricardomartins.net.br>
2017-10-20 15:17:12 +1100

    Fixing acceptable currency

--- app/code/RicardoMartins/PagSeguro/Model/Payment.php
+++ app/code/RicardoMartins/PagSeguro/Model/Payment.php
@@ -28,7 +28,7 @@ class Payment extends \Magento\Payment\Model\Method\Cc
 
     protected $_minAmount = null;
     protected $_maxAmount = null;
-    protected $_supportedCurrencyCodes = array('USD');
+    protected $_supportedCurrencyCodes = array('BRL');
 
     protected $_debugReplacePrivateDataKeys = ['number', 'exp_month', 'exp_year', 'cvc'];
     /**
