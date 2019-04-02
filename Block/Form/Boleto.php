398a9112bb25cdb7970794aead7b9303dced1d09
Ricardo Martins <ricardo@ricardomartins.net.br>
2019-04-02 11:02:20 +1100

    Moving forward on boleto

---
+++ Block/Form/Boleto.php
@@ -0,0 +1,10 @@
+<?php
+namespace RicardoMartins\PagSeguro\Block\Form;
+
+class Boleto extends \Magento\Payment\Block\Form
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'RicardoMartins_PagSeguro::form/boleto.phtml';
+}
