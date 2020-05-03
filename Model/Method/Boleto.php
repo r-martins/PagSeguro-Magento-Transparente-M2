109d16e9869324cbdea388d110e623c24bafd378
dev local <dev@fmdevelopment.com>
2020-05-04 04:47:46 +0530

    fix: get additional payment data from assignData function for method boleto

--- Model/Method/Boleto.php
+++ Model/Method/Boleto.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RicardoMartins\PagSeguro\Model\Method;
 
 /**
@@ -10,178 +11,174 @@ namespace RicardoMartins\PagSeguro\Model\Method;
  * @license   https://www.gnu.org/licenses/gpl-3.0.pt-br.html GNU GPL, version 3
  * @package   RicardoMartins\PagSeguro\Model\Method
  */
-class Boleto extends \Magento\Payment\Model\Method\AbstractMethod
-{
+class Boleto extends \Magento\Payment\Model\Method\AbstractMethod {
+
     /**
      * @var string
      */
     const CODE = 'rm_pagseguro_boleto';
+
     protected $_code = self::CODE;
-    protected $_isGateway                   = true;
-    protected $_canCapture                  = true;
-    protected $_canCapturePartial           = true;
-    protected $_canRefund                   = true;
-    protected $_canRefundInvoicePartial     = true;
+    protected $_isGateway = true;
+    protected $_canCapture = true;
+    protected $_canCapturePartial = true;
+    protected $_canRefund = true;
+    protected $_canRefundInvoicePartial = true;
     protected $_countryFactory;
     protected $_minAmount = null;
     protected $_maxAmount = null;
     protected $_supportedCurrencyCodes = array('BRL');
     protected $_infoBlockType = \RicardoMartins\PagSeguro\Block\Payment\Info::class;
-
     protected $_debugReplacePrivateDataKeys = ['number', 'exp_month', 'exp_year', 'cvc'];
+
     /**
      * PagSeguro Helper
      *
      * @var RicardoMartins\PagSeguro\Helper\Data;
-     */ 
+     */
     protected $pagSeguroHelper;
 
     /**
      * PagSeguro Abstract Model
      *
      * @var RicardoMartins\PagSeguro\Model\Notifications
-     */ 
+     */
     protected $pagSeguroAbModel;
 
     /**
      * Backend Auth Session
      *
      * @var Magento\Backend\Model\Auth\Session $adminSession
-     */ 
+     */
     protected $adminSession;
 
     /** @var \Magento\Framework\Message\ManagerInterface */
     protected $messageManager;
 
     public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
-        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
-        \Magento\Payment\Helper\Data $paymentData,        
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Payment\Model\Method\Logger $logger,
-        \RicardoMartins\PagSeguro\Helper\Data $pagSeguroHelper,
-        \RicardoMartins\PagSeguro\Model\Notifications $pagSeguroAbModel,
-        \Magento\Backend\Model\Auth\Session $adminSession,        
-        array $data = array()
+            \Magento\Framework\Model\Context $context,
+            \Magento\Framework\Registry $registry,
+            \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+            \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+            \Magento\Payment\Helper\Data $paymentData,
+            \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+            \Magento\Payment\Model\Method\Logger $logger,
+            \RicardoMartins\PagSeguro\Helper\Data $pagSeguroHelper,
+            \RicardoMartins\PagSeguro\Model\Notifications $pagSeguroAbModel,
+            \Magento\Backend\Model\Auth\Session $adminSession,
+            array $data = array()
     ) {
         parent::__construct(
-            $context,
-            $registry,
-            $extensionFactory,
-            $customAttributeFactory,
-            $paymentData,
-            $scopeConfig,
-            $logger,
-            null,
-            null,
-            $data
+                $context,
+                $registry,
+                $extensionFactory,
+                $customAttributeFactory,
+                $paymentData,
+                $scopeConfig,
+                $logger,
+                null,
+                null,
+                $data
         );
 
-        $this->pagSeguroHelper = $pagSeguroHelper;  
-        $this->pagSeguroAbModel = $pagSeguroAbModel; 
+        $this->pagSeguroHelper = $pagSeguroHelper;
+        $this->pagSeguroAbModel = $pagSeguroAbModel;
         $this->adminSession = $adminSession;
     }
-    
+
     /**
      * Assign data to info model instance
      *
      * @param   mixed $data
      * @return  object
      */
-    public function assignData(\Magento\Framework\DataObject $data)
-    {
+    public function assignData(\Magento\Framework\DataObject $data) {
         if (!$data instanceof \Magento\Framework\DataObject) {
             $data = new \Magento\Framework\DataObject($data);
         }
-
+        
         $info = $this->getInfoInstance();
         $info->setAdditionalInformation('sender_hash', $this->pagSeguroHelper->getPaymentHash('sender_hash'));
-        
+
         //$this->pagSeguroHelper->writeLog('getData Order'. print_r($data->getData()));
         // set cpf
         if ($this->pagSeguroHelper->isCpfVisible()) {
-			
-			$this->pagSeguroHelper->writeLog('boletocpf_cpf'. $this->pagSeguroHelper->getCCOwnerData('boleto_cpf'));
-			
-            $info->setAdditionalInformation($this->getCode() . '_cpf', $this->pagSeguroHelper->getCCOwnerData('boleto_cpf'));
+
+            $this->pagSeguroHelper->writeLog('boletocpf_cpf' . $data['additional_data']['boleto_cpf']);
+
+            $info->setAdditionalInformation($this->getCode() . '_cpf', $data['additional_data']['boleto_cpf']);
         }
-        
+
         return $this;
     }
-    
-    public function order(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
+
+    public function order(\Magento\Payment\Model\InfoInterface $payment, $amount) {
         //@TODO Review. Necessary?
-          /*@var \Magento\Sales\Model\Order $order */
-          $this->pagSeguroHelper->writeLog('Inside Order');
-          
-          /*@var \Magento\Sales\Model\Order $order */
-			$order = $payment->getOrder();
+        /* @var \Magento\Sales\Model\Order $order */
+        $this->pagSeguroHelper->writeLog('Inside Order');
 
-			try {
+        /* @var \Magento\Sales\Model\Order $order */
+        $order = $payment->getOrder();
 
-				//will grab data to be send via POST to API inside $params
-				$params = $this->pagSeguroHelper->getBoletoApiCallParams($order, $payment);
-                $this->pagSeguroHelper->writeLog($params);
+        try {            
+            //will grab data to be send via POST to API inside $params
+            $params = $this->pagSeguroHelper->getBoletoApiCallParams($order, $payment);
+            $this->pagSeguroHelper->writeLog($params);
 
-				//call API
-				$returnXml = $this->pagSeguroHelper->callApi($params, $payment);
+            //call API
+            $returnXml = $this->pagSeguroHelper->callApi($params, $payment);
 
-				if (isset($returnXml->errors)) {
-					$errMsg = array();
-					foreach ($returnXml->errors as $error) {
-					    $message = $this->pagSeguroHelper->translateError((string)$error->message);
-						$errMsg[] = $message . '(' . $error->code . ')';
-					}
-					throw new \Magento\Framework\Validator\Exception('Um ou mais erros ocorreram no seu pagamento.' . PHP_EOL . implode(PHP_EOL, $errMsg));
-				}
-				if (isset($returnXml->error)) {
-					$error = $returnXml->error;
-                    $message = $this->pagSeguroHelper->translateError((string)$error->message);
-					$errMsg[] = $message . ' (' . $error->code . ')';
-					throw new \Magento\Framework\Validator\Exception('Um erro ocorreu em seu pagamento.' . PHP_EOL . implode(PHP_EOL, $errMsg));
-				}
-				/* process return result code status*/
-				if ((int)$returnXml->status == 6 || (int)$returnXml->status == 7) {
-					throw new \Magento\Framework\Validator\Exception('An error occurred in your payment.');
-				}
+            if (isset($returnXml->errors)) {
+                $errMsg = array();
+                foreach ($returnXml->errors as $error) {
+                    $message = $this->pagSeguroHelper->translateError((string) $error->message);
+                    $errMsg[] = $message . '(' . $error->code . ')';
+                }
+                throw new \Magento\Framework\Validator\Exception('Um ou mais erros ocorreram no seu pagamento.' . PHP_EOL . implode(PHP_EOL, $errMsg));
+            }
+            if (isset($returnXml->error)) {
+                $error = $returnXml->error;
+                $message = $this->pagSeguroHelper->translateError((string) $error->message);
+                $errMsg[] = $message . ' (' . $error->code . ')';
+                throw new \Magento\Framework\Validator\Exception('Um erro ocorreu em seu pagamento.' . PHP_EOL . implode(PHP_EOL, $errMsg));
+            }
+            /* process return result code status */
+            if ((int) $returnXml->status == 6 || (int) $returnXml->status == 7) {
+                throw new \Magento\Framework\Validator\Exception('An error occurred in your payment.');
+            }
 
-				$payment->setSkipOrderProcessing(true);
+            $payment->setSkipOrderProcessing(true);
 
-				if (isset($returnXml->code)) {
+            if (isset($returnXml->code)) {
 
-					$additional = array('transaction_id'=>(string)$returnXml->code);
-					if ($existing = $payment->getAdditionalInformation()) {
-						if (is_array($existing)) {
-							$additional = array_merge($additional, $existing);
-						}
-					}
-					$payment->setAdditionalInformation($additional);
-					
-					if (isset($returnXml->paymentMethod->type) && (int)$returnXml->paymentMethod->type == 2) {
-					   $payment->setAdditionalInformation('boletoUrl', (string)$returnXml->paymentLink);
-					}
+                $additional = array('transaction_id' => (string) $returnXml->code);
+                if ($existing = $payment->getAdditionalInformation()) {
+                    if (is_array($existing)) {
+                        $additional = array_merge($additional, $existing);
+                    }
+                }
+                $payment->setAdditionalInformation($additional);
 
-				}
+                if (isset($returnXml->paymentMethod->type) && (int) $returnXml->paymentMethod->type == 2) {
+                    $payment->setAdditionalInformation('boletoUrl', (string) $returnXml->paymentLink);
+                }
+            }
 
-			  $this->pagSeguroAbModel->proccessNotificatonResult($returnXml, $payment);
-			} catch (\Exception $e) {
+            $this->pagSeguroAbModel->proccessNotificatonResult($returnXml, $payment);
+        } catch (\Exception $e) {
 
-				throw new \Magento\Framework\Exception\LocalizedException(__($e->getMessage()));
-			}
-			return $this;
+            throw new \Magento\Framework\Exception\LocalizedException(__($e->getMessage()));
+        }
+        return $this;
     }
 
-	/**
+    /**
      * Determine method availability based on quote amount and config data
      *
      * @param \Magento\Quote\Api\Data\CartInterface|null $quote
      * @return bool
      */
-    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
-    {
+    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null) {
         if ($this->adminSession->getUser()) {
             return false;
         }
@@ -202,4 +199,5 @@ class Boleto extends \Magento\Payment\Model\Method\AbstractMethod
 
         return false;
     }
+
 }
