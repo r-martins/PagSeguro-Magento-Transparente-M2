1448a069f54eeba89bba0422c4b5995ada28a8cc
Ricardo Martins <ricardo@ricardomartins.net.br>
2020-12-23 12:28:28 +1100

    ajustes no reembolso (padroes de codigo e outros detalhes)

--- Observer/CreditMemoObserver.php
+++ Observer/CreditMemoObserver.php
@@ -2,46 +2,47 @@
 
 namespace RicardoMartins\PagSeguro\Observer;
 
+use Magento\Backend\Model\Auth\Session;
+use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Model\Order\Status\HistoryFactory;
 use Magento\Sales\Api\OrderRepositoryInterface;
 
 class CreditMemoObserver implements ObserverInterface
 {
-    protected $_order;
-    protected $orderHistoryFactory;
-    protected $orderRepository;
-    protected $authSession;
+    protected OrderRepositoryInterface $_order;
+    protected HistoryFactory $orderHistoryFactory;
+    protected OrderRepositoryInterface $orderRepository;
+    protected Session $authSession;
 
     public function __construct(
         OrderRepositoryInterface $order,
         HistoryFactory $orderHistoryFactory,
         OrderRepositoryInterface $orderRepository,
-        \Magento\Backend\Model\Auth\Session $authSession
-    )
-    {
+        Session $authSession
+    ) {
         $this->_order = $order;
         $this->orderHistoryFactory = $orderHistoryFactory;
         $this->orderRepository = $orderRepository;
         $this->authSession = $authSession;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(Observer $observer): CreditMemoObserver
     {
         $refund = $observer->getEvent()->getCreditmemo();
         $_order = $refund->getOrder();
         $payment = $_order->getPayment();
         $method = $payment->getMethod();
 
-        if(stristr($method,"rm_pagseguro")) {
+        if (stristr($method, "rm_pagseguro")) {
             $orderId = $_order->getId();
             $order = $this->orderRepository->get($orderId);
             $value = $refund->getGrandTotal();
             $user = $this->authSession->getUser();
             $user->getName();
             $user->getId();
-            $comment = "Reembolso de R$".number_format($value,2,",",".")
-                . " por ".$user->getName()." (ID ".$user->getId().")";
+            $comment = "Reembolso de R$" . number_format($value, 2, ",", ".")
+                . " feito por " . $user->getName() . " (" . $user->getUserName() . ")";
             $this->orderHistoryFactory->create();
             $history = $this->orderHistoryFactory->create()
                 ->setStatus($order->getStatus())
