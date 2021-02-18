bf1d9e2bb7f75864b8594a3e57dd29facb610f77
Fillipe <fillipe@querycommerce.com>
2021-02-18 14:33:41 -0300

    Limpeza de cache da PagSeguro - relacionado as informacoes de juros armazenadas nos produtos - passa a conferir se existem produtos no catalogo antes de realizar atualizacao nos atributos.

--- Model/Cache/Type/Interests.php
+++ Model/Cache/Type/Interests.php
@@ -45,42 +45,28 @@ class Interests extends TagScope
         );
         $this->eavConfig = $eavConfig;
     }
+
+    /**
+     * 
+     */
     public function clean($mode = \Zend_Cache::CLEANING_MODE_ALL, array $tags = [])
     {
-        if (!$this->productAttributeExists('rm_interest_options')
-            || !$this->productAttributeExists('rm_pagseguro_last_update')) {
+        // checks if interest attributes exist
+        if (!$this->productAttributeExists('rm_interest_options') ||
+            !$this->productAttributeExists('rm_pagseguro_last_update'))
+        {
             return;
         }
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection */
-        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
-        /** Apply filters here */
-        $collection = $productCollection->addAttributeToSelect('*')
-            ->load();
-        $productIds = [];
-        foreach($collection as $product){
-            $productIds[] = $product->getId();
-        }
-        $productActionObject = $objectManager->create('Magento\Catalog\Model\Product\Action');
-        $productActionObject->updateAttributes($productIds, array('rm_interest_options' => ""), 0);
-        $productActionObject->updateAttributes($productIds,array('rm_pagseguro_last_update' => 0), 0);
+
+        $this->_resetInterestProductAttributes();
     }
 
+    /**
+     * 
+     */
     public function flush($mode = \Zend_Cache::CLEANING_MODE_ALL, array $tags = [])
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection */
-        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
-        /** Apply filters here */
-        $collection = $productCollection->addAttributeToSelect('*')
-            ->load();
-        $productIds = [];
-        foreach($collection as $product){
-            $productIds[] = $product->getId();
-        }
-        $productActionObject = $objectManager->create('Magento\Catalog\Model\Product\Action');
-        $productActionObject->updateAttributes($productIds, array('rm_interest_options' => ""), 0);
-        $productActionObject->updateAttributes($productIds,array('rm_pagseguro_last_update' => 0),0);
+        $this->_resetInterestProductAttributes();
     }
 
     /**
@@ -97,4 +83,31 @@ class Interests extends TagScope
         return ($attr && $attr->getId());
     }
 
+    /**
+     * Reset interest product 
+     * attributes values
+     */
+    private function _resetInterestProductAttributes()
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        
+        /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection */
+        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
+
+        // only update attributes if 
+        // there are products on catalog
+        if($productCollection->getSize() > 0)
+        {
+            $productIds = [];
+            
+            foreach($productCollection as $product)
+            {
+                $productIds[] = $product->getId();
+            }
+            
+            $productActionObject = $objectManager->create('Magento\Catalog\Model\Product\Action');
+            $productActionObject->updateAttributes($productIds, array('rm_interest_options' => ""), 0);
+            $productActionObject->updateAttributes($productIds,array('rm_pagseguro_last_update' => 0), 0);
+        }
+    }
 }
