416d0bee48621aa717055468ffb8bc5b898a9d92
Ricardo Martins <ricardo@ricardomartins.net.br>
2021-10-16 11:30:10 +1100

    * Correção: quando um espaço era adicionado em um dos campos de data de nascimento do titular do cartão de crédito, o valor 01/01/1970 era enviado para o pagseguro, distorcendo a análise antifraude e aumentando as chances do pedido ser negado. (Reportado por Bruna - Sunsetcosmeticos)

--- Model/Method/Cc.php
+++ Model/Method/Cc.php
@@ -259,9 +259,15 @@ class Cc extends \Magento\Payment\Model\Method\Cc
 
         //DOB value
         if ($this->pagSeguroHelper->isDobVisible()) {
-            $dobDay = $data['additional_data']['cc_owner_birthday_day'] ?? '01';
-            $dobMonth = $data['additional_data']['cc_owner_birthday_month'] ?? '01';
-            $dobYear = $data['additional_data']['cc_owner_birthday_year'] ?? '1970';
+            $dobDay = $data['additional_data']['cc_owner_birthday_day'] ? trim(
+                $data['additional_data']['cc_owner_birthday_day']
+            ) : '01';
+            $dobMonth = $data['additional_data']['cc_owner_birthday_month'] ? trim(
+                $data['additional_data']['cc_owner_birthday_month']
+            ) : '01';
+            $dobYear = $data['additional_data']['cc_owner_birthday_year'] ? trim(
+                $data['additional_data']['cc_owner_birthday_year']
+            ) : '1970';
             $info->setAdditionalInformation(
                 'credit_card_owner_birthdate',
                 date(
