392c5346d471fa528ddd9a7565f9ad5ea71e4d74
Fillipe <fillipe@querycommerce.com>
2021-10-27 19:33:42 -0300

    Aquisicao do token do cartao passa a ocorrer quando o usuario clica em Finalizar Compra (formulario de 1 cartao)

--- view/frontend/web/js/view/payment/method-renderer/rm_pagseguro_ccmethod.js
+++ view/frontend/web/js/view/payment/method-renderer/rm_pagseguro_ccmethod.js
@@ -2,13 +2,14 @@ define(
     [
         'Magento_Payment/js/view/payment/cc-form',
         'jquery',
-        'Magento_Checkout/js/action/place-order',
         'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Ui/js/model/messageList',
+        'Magento_Checkout/js/action/place-order',
         'Magento_Checkout/js/model/payment/additional-validators',
         'Magento_Payment/js/model/credit-card-validation/validator',
         'PagseguroDirectMethod'
     ],
-    function (Component, $) {
+    function (Component, $, fullScreenLoader, globalMessageList) {
         'use strict';
 
         return Component.extend({
@@ -19,7 +20,8 @@ define(
                 creditCardOwnerBirthMonth: '',
                 creditCardOwnerBirthYear: '',
                 creditCardOwnerCpf: '',
-                creditCardInstallments: ''
+                creditCardInstallments: '',
+                disablePlaceOrderButton: false
             },
 
             getPagSeguroCcImagesHtml: function () {
@@ -34,7 +36,8 @@ define(
                         'creditCardOwnerBirthMonth',
                         'creditCardOwnerBirthYear',
                         'creditCardOwnerCpf',
-                        'creditCardInstallments'
+                        'creditCardInstallments',
+                        'disablePlaceOrderButton'
                     ]);
 
                 return this;
@@ -92,6 +95,70 @@ define(
                     return false;
                 }
                 return true;
+            },
+
+            placeOrder: function(data, event) {
+                if (this.validate()) {
+                    fullScreenLoader.startLoader();
+                    this.disablePlaceOrderButton(true);
+
+                    this.RMPagSeguroObj.updateOneCreditCardToken(
+                        // success callback function
+                        this._super.bind(this, data, event),
+
+                        // error callback function
+                        (function(errors) {
+                            let formattedErrors = [];
+
+                            _.each(errors, function(value, errorCode) {
+                                switch (errorCode) {
+                                    case '30400':
+                                        formattedErrors.push('Verifique número, data de validade e CVV.');
+                                        break;
+                                    case '10001':
+                                        formattedErrors.push('Tamanho do cartão inválido.');
+                                        break;
+                                    case '10006':
+                                        formattedErrors.push('Tamanho do CVV inválido.');
+                                        break;
+                                    case '30405':
+                                        formattedErrors.push('Data de validade incorreta.');
+                                        break;
+                                    case '11157':
+                                        formattedErrors.push('CPF inválido.');
+                                        break;
+                                }
+                            });
+
+                            if (formattedErrors.length == 0) {
+                                formattedErrors.push('Verifique os dados do cartão.');
+                            }
+
+                            let errorMessage = 'Dados do cartão inválidos. ' + formattedErrors.join(' ');
+                            let messageContainer = $('#pagseguro_cc_method div.messages');
+                            let errorTemplate = 
+                                `<div role="alert" class="message message-error error">
+                                    <div data-ui-id="checkout-cart-validationmessages-message-error">
+                                        ${errorMessage}
+                                    </div>
+                                </div>`;
+                            
+                            // shows error and scroll to it
+                            messageContainer.html(errorTemplate);
+                            $('html, body').animate({
+                                scrollTop: messageContainer.offset().top - 20
+                            }, 1000);
+                        }).bind(this),
+
+                        // complete callback function
+                        (function() {
+                            fullScreenLoader.stopLoader();
+                            this.disablePlaceOrderButton(false);
+                        }).bind(this)
+                    );
+                }
+
+                return false;
             }
         });
     }
