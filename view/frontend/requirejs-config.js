c59493d25ab8db4ea1d18ffea6d37a8676a8665a
Fillipe <fillipe@querycommerce.com>
2021-11-04 20:23:22 -0300

    Remocao de mixin desnecessario

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,11 +6,10 @@ var config = {
     },
     config: {
         // Disables the firecheckout mixin on payment service model of Magento Checkout.
-        // Its avoids bugs when coupon is applied / cancenled on checkout page.
+        // This avoids bugs when coupon is applied / cancenled on checkout page.
         mixins: {
             'Magento_Checkout/js/model/payment-service': {
-                'Swissup_Firecheckout/js/mixin/model/payment-service-mixin': false,
-                'RicardoMartins_PagSeguro/js/mixin/model/firecheckout-fix': true
+                'Swissup_Firecheckout/js/mixin/model/payment-service-mixin': false
             }
         }
     }
