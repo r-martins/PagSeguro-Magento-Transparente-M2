13a3c6b8b589de052ff11ffc9101681923300339
Ligia Salzano <salzano.ligia@trezo.com.br>
2022-12-27 22:28:45 -0300

    Validate public key field #183810824

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'RicardoMartins_PagSeguro/js/validation-mixin': true
+            }
+        }
+    }
+};
---
+++ view/adminhtml/web/js/validation-mixin.js
@@ -0,0 +1,14 @@
+define(['jquery'], function($) {
+    'use strict';
+
+    return function(targetWidget) {
+        $.validator.addMethod(
+            'validate-pagseguro-public-key',
+            function(value, element) {
+                return value.length === 35;
+            },
+            $.mage.__('Check your public key. It has 35 characters.')
+        )
+        return targetWidget;
+    }
+});
